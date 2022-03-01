285c7179562c4b40c9c159dc1754f07531d062e3
brAndrey <train1284@mail.ru>
2022-03-01 13:58:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/git_hub_2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.git_hub_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //1
+
+    }
+}
